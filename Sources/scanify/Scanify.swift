8010908865c0f94e53279b61f41837ee8450fb5a
Bragadeesh <bragboy@gmail.com>
2025-12-06 20:14:21 +0400

    Version is 1.0.0

--- Sources/scanify/Scanify.swift
+++ Sources/scanify/Scanify.swift
@@ -6,7 +6,7 @@ struct Scanify: ParsableCommand {
     static let configuration = CommandConfiguration(
         commandName: "scanify",
         abstract: "Transform PDFs to look like scanned documents",
-        version: "0.1.0"
+        version: "1.0.0"
     )
 
     @Flag(name: .long, help: "Apply aggressive scan effects (more noise, rotation, artifacts)")
